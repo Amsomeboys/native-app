61b7065f006c6a693d277e497ea726a60ad20e81
Amsomeboys <khnun1090@gmail.com>
2023-07-05 19:13:25 +0700

    create component

--- App.js
+++ App.js
@@ -1,20 +1,125 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { useState } from 'react';
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
+import Images from './assets/image/cat.png';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 65,
+    marginBottom: 30,
   },
 });
+
+const App = () => {
+  const [num, setNum] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  return (
+    <>
+      <ScrollView style={styles.container}>
+        <View style={{ alignItems: 'center', marginBottom: 20 }}>
+          <Text style={{ fontSize: 20 }}>isLoading?</Text>
+          <Button
+            title="Toggle"
+            color="green"
+            onPress={() => {
+              setIsLoading(!isLoading);
+            }}
+          ></Button>
+          {isLoading ? <ActivityIndicator size="large" color="orange" /> : ''}
+        </View>
+        <View style={{ marginBottom: 20, alignItems: 'center' }}>
+          <Text style={{ marginBottom: 20 }}>Button {num}</Text>
+          <Button
+            title="plus"
+            onPress={() => {
+              setNum(num + 1);
+            }}
+          />
+        </View>
+        <View style={{ marginBottom: 20, alignItems: 'center' }}>
+          <Button
+            title="minus"
+            onPress={() => {
+              {
+                num === 0 ? 0 : setNum(num - 1);
+              }
+            }}
+          />
+        </View>
+        <View style={{ marginBottom: 20, alignItems: 'center' }}>
+          <Button
+            title="reset"
+            onPress={() => {
+              setNum(0);
+            }}
+          />
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <Text style={{ fontSize: 20, marginBottom: 10, alignSelf: 'center' }}>
+            TextInput
+          </Text>
+          <TextInput
+            style={{ height: 40, borderWidth: 1, paddingLeft: 10 }}
+            placeholder="type here"
+          />
+        </View>
+        <View style={{ alignItems: 'center' }}>
+          <TouchableOpacity
+            onPress={() => {
+              setNum(num + 10);
+            }}
+          >
+            <Text style={{ width: 150, textAlign: 'center', borderWidth: 1 }}>
+              Power Up
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{ alignItems: 'center', marginBottom: 20 }}>
+          <Text style={{ fontSize: 20, marginBottom: 10 }}>Image(uri)</Text>
+          <TouchableOpacity
+            onPress={() => {
+              console.log(num), setNum(888);
+            }}
+          >
+            <Image
+              source={{
+                uri: 'https://cdn.marvel.com/content/1x/guardiansofthegalaxyvolume3_lob_crd_03.jpg',
+              }}
+              style={{ height: 400, width: 200 }}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={{ alignItems: 'center', marginBottom: 20 }}>
+          <Text style={{ fontSize: 20, marginBottom: 10 }}>Image(src)</Text>
+          <TouchableOpacity
+            onPress={() => {
+              console.log(num), setNum(999);
+            }}
+          >
+            <Image source={Images} style={{ height: 400, width: 200 }} />
+          </TouchableOpacity>
+        </View>
+        <View style={{ alignItems: 'center' }}>
+          <Text style={{ fontSize: 20 }}>Text:</Text>
+          <Text style={{ fontSize: 24 }}>Hello Moto!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </ScrollView>
+    </>
+  );
+};
+
+export default App;
